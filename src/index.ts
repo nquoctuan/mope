b561342b77e736c9edb670d3addacf4a84574f52
Jimmy Nguyen <tuan.nguyen@jayeson.com.sg>
2017-08-01 12:42:45 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import  * as express from 'express';
+
+const app: express.Express = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+
+    let person = { name : 'quan' };
+
+    person = Object.assign(person, req.query);
+
+    res.json(person);
+});
+
+app.listen('3002',() => console.log('Server listening on port 3002'));  
